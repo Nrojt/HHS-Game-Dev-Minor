137b47738e34aad5a23c320ccbdbcff0f209fc8e
Tjorn <74906283+Nrojt@users.noreply.github.com>
2025-02-12 10:52:39 +0100

    tilemap collisions coyote time

--- finite-state-machine/player/scripts/player.gd
+++ finite-state-machine/player/scripts/player.gd
@@ -1,25 +1,40 @@
 extends CharacterBody2D
 
+@export var SPEED: float = 100.0
+@export var JUMP_VELOCITY: float = -300.0
+@export var COYOTE_TIME: float = 0.1
+@export var MAX_JUMPS: int = 2
 
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -400.0
+@onready var coyote_timer := $CoyoteTimer
+
+var jumps_available: int = MAX_JUMPS
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle jump
+	if Input.is_action_just_pressed("jump") and jumps_available > 0:
+		jumps_available -= 1
 		velocity.y = JUMP_VELOCITY
 
+	# Add the gravity.
+	if not is_on_floor():
+		if jumps_available > 0 and coyote_timer.is_stopped():
+			coyote_timer.start(COYOTE_TIME)
+		velocity += get_gravity() * delta
+	else:
+		coyote_timer.stop()
+		jumps_available = MAX_JUMPS
+
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction: float = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_coyote_timer_timeout():
+	if not is_on_floor() and jumps_available > 0:
+		jumps_available -= 1
